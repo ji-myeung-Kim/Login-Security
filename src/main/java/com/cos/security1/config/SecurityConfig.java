30db776d9be7decc912b63e71cb05e5878b50922
kjm <wlaud5314@naver.com>
2021-11-14 21:59:15 +0900

    [Add] oauth2

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,6 +1,12 @@
 package com.cos.security1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+//import org.springframework.beans.factory.annotation.Autowired;
+//1. 코드받기(인증), 2.엑세스토큰(권한), 
+//3.사용자프로필 정보를 가져오고 4. 그 정보를 토대로 회원가입을 자동으로 진행 
+// 4-2 (이메일, 전화번호, 이름 아이디) 쇼핑몰 ->(집주소), 백화점몰 -> (vip등급, 일반등급)
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,10 +14,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+
 @Configuration
 @EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)//secured어노테이션 활성화, preAuthorize,postAturhoize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	@Autowired
+	private PrincipalOauth2UserService principalOauth2UserService;
 	
 	@Bean //해당 메서드의 리턴되는 오브젝트를 IOC로 등록해준다
 	public BCryptPasswordEncoder encodePwd() {
@@ -31,8 +43,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.formLogin()
 			.loginPage("/loginForm")
 			.loginProcessingUrl("/login") //login주소가 호출이 되면 시큐리티가 낚아차서 대신 로그인 진행
-			.defaultSuccessUrl("/");
-
+			.defaultSuccessUrl("/")
+			.and()
+			.oauth2Login()
+			.loginPage("/loginForm")
+			.userInfoEndpoint()
+			.userService(principalOauth2UserService);// 구글 로그인이 완료된 뒤의 후처리가 필요함. Tip.코드(엑세스 토큰+사용자프로필 정보 O)
 	}
 	
 }
