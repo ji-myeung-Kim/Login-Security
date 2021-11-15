272259d3d1ef69569cded744fdb06762451d21a4
kjm <wlaud5314@naver.com>
2021-11-15 23:17:34 +0900

    [Add] NaverLogin

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,12 +3,16 @@ package com.cos.security1.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cos.security1.config.auth.PrincipalDetails;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 
@@ -30,8 +34,34 @@ public class IndexController {
 		return "index"; // src/main/resources/templates(index.mustache)
 	}
 	
+	@GetMapping("/test/login")
+	public @ResponseBody String testLogin(Authentication authentication, @AuthenticationPrincipal PrincipalDetails userDetails) {//Di(의존성주입)
+		System.out.println("/test/login==========");
+		PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+		System.out.println("/authentication: "+ principalDetails.getUser());
+		
+		
+		System.out.println("userDetails: " + userDetails.getUser());
+		return "세션정보 확인하기";
+	}
+	
+	@GetMapping("/test/oauth/login")
+	public @ResponseBody String testOAuthLogin(
+			Authentication authentication,
+			@AuthenticationPrincipal OAuth2User oauth) {//Di(의존성주입)
+		System.out.println("/test/oauth/login==========");
+		OAuth2User oauth2User = (OAuth2User)authentication.getPrincipal();
+		System.out.println("/authentication: "+ oauth2User.getAttributes());
+		
+		System.out.println("oauth2User:" + oauth.getAttributes() );
+		return "OAuth세션정보 확인하기";
+	}
+	
+//	OAuth 로그인을 해도 PrincipalDetails
+//	일반 로그인을 해도 PrincipalDetails
 	@GetMapping("/user")
-	public @ResponseBody String user() {
+	public @ResponseBody String user(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+		System.out.println("principalDetails: " + principalDetails.getUser());
 		return "user";
 	}
 	
